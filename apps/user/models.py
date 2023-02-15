44586c461ee2a2007ed06ec2d2c4dad02ee48299
Valss22 <valsshokorov@gmail.com>
2023-02-15 22:14:00 +0600

    recievers fixed

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
 
