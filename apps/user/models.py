0d69c89679950fbad4c94b1712a60476cba971f3
Valss22 <valsshokorov@gmail.com>
2023-02-16 16:30:11 +0600

    unique true added

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.user.username
