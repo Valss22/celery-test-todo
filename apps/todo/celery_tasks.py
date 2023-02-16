8d32532f6aa6c676fc39ec1baf607738673dad74
Valss22 <valsshokorov@gmail.com>
2023-02-16 16:37:12 +0600

    removed comment

--- apps/todo/celery_tasks.py
+++ apps/todo/celery_tasks.py
@@ -1,6 +1,4 @@
 from django.core.mail import send_mail
-from time import sleep
-
 from celery import shared_task
 
 from config.settings import EMAIL_HOST
@@ -8,5 +6,4 @@ from config.settings import EMAIL_HOST
 
 @shared_task()
 def send_email_task(subject: str, message: str, email_recipient: list[str]):
-    sleep(3)  # чисто ради проверки что она не блокирует основной поток
     send_mail(subject, message, EMAIL_HOST, email_recipient)
